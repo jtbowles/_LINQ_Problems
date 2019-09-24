5d4c36918633c00f3606fe898c37bcfec0582454
Jason Bowles <jbowles96@gmail.com>
2019-09-24 09:47:29 -0500

    LINQ problems using .Contains() to return strings that contain a specific substring, using .Select().Distinct() to return only the first occurance of an element

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -10,7 +10,32 @@ namespace LINQProblems
     {
         static void Main(string[] args)
         {
+            // LINQ problem 1
 
+            //var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            //var wordsWithTH = words.Where(w => w.Contains("th"));
+            //foreach (var word in wordsWithTH)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+
+            // LINQ problem 2
+
+            //var names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            //var namesWithoutDuplicates = names.Select(n => n).Distinct();
+            //foreach (var name in namesWithoutDuplicates)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+            // LINQ problem 3
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
